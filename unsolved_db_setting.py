c341c3810d8531515b2630e6d76b1e1391b4d137
siontama <siontama@naver.com>
2022-06-23 14:13:17 +0900

    Feat: table이 존재하지 않을시 생성

---
+++ unsolved_db_setting.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pymysql
+
+
+def db_setting():
+    db = pymysql.connect(
+        user='root',
+        passwd='990312aa',
+        host='localhost',
+        db='unsolved',
+    )
+    cur = db.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS problem('
+                'id INTEGER PRIMARY KEY AUTO_INCREMENT,'
+                'boj_id BIGINT,'
+                'title VARCHAR(50),'
+                'tier INTEGER,'
+                'created_at DATETIME DEFAULT CURRENT_TIMESTAMP,'
+                'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP )')
+
+    db.commit()
+    cur.close()
+    db.close()
